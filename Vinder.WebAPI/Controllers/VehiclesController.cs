ae41085774e693f932211c211e6e99eae72edb9d
virusstorm <tcappelletti@gmail.com>
2016-01-20 12:40:45 -0500

    Finished API stub

--- Vinder.WebAPI/Controllers/VehiclesController.cs
+++ Vinder.WebAPI/Controllers/VehiclesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -12,35 +13,48 @@ namespace Vinder.WebAPI.Controllers
     public class VehiclesController : ApiController
     {
         [Route("")]
-        public Vehicle[] GetVehicles()
+        public Vehicle[] GetVehicles(
+            [FromUri] string uid,
+            [FromUri] string[] vins,
+            [FromUri] double? latitude,
+            [FromUri] double? longitude)
         {
-            return new Vehicle[]
-            {
-                new Vehicle()
-                {
-                    AttributesDictionary = new Dictionary<string, string>{ { "Make", "Jeep" } },
-                    Distance = 10.0m,
-                    Images = new []{"Test1.jpg"},
-                    MasterImage = "MasterImage.jpg",
-                    Vin = "12345678901234567"
-                },
-            };
+            return GenerateDummyVehicles(uid, vins, latitude, longitude);
         }
 
-        [Route("{latitude:double}/{longitude:double}")]
-        public Vehicle[] GetLocalVehicles(double latitude, double longitude)
+        private Vehicle[] GenerateDummyVehicles(string uid, string[] vins, double? latitude, double? longitude)
         {
-            return new Vehicle[]
+            var vehicles = new List<Vehicle>();
+
+            var vehicle = new Vehicle()
             {
-                new Vehicle()
-                {
-                    AttributesDictionary = new Dictionary<string, string>{ { "Make", "Jeep" } },
-                    Distance = 10.0m,
-                    Images = new []{"Test1.jpg"},
-                    MasterImage = "MasterImage.jpg",
-                    Vin = "12345678901234567"
-                },
+                AttributesDictionary = new Dictionary<string, string>(),
+                Distance = 10.0m,
+                Images = new[] { "Test1.jpg", "Test2.jpg", "Test3.jpg" },
+                MasterImage = "MasterImage.jpg",
+                Vin = "12345678901234567"
             };
+
+            if (vins != null)
+            {
+                int index = 0;
+
+                foreach (var vin in vins)
+                {
+                    vehicle.AttributesDictionary.Add("ExcludeVin" + index, vin);
+                    index++;
+                }
+            }
+
+            vehicle.AttributesDictionary.Add("uid", uid);
+
+            vehicle.AttributesDictionary.Add("latitude", latitude.HasValue ? latitude.Value.ToString(CultureInfo.CurrentCulture) : null);
+
+            vehicle.AttributesDictionary.Add("longitude", longitude.HasValue ? longitude.Value.ToString(CultureInfo.CurrentCulture) : null);
+
+            vehicles.Add(vehicle);
+
+            return vehicles.ToArray();
         }
     }
 }
